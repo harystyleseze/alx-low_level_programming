0a1a096f1eda1d9a0f77d453b7736c0000f58093
harystyles <harystyleseze@gmail.com>
2023-09-15 14:32:49 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,28 +9,25 @@
  *
  * Return: void always
  */
-
 void print_diagonal(int n)
 {
-	int i;
-	int j;
+    int i;
+    int j;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		while (i <= n)
-		{
-			for (j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-			i++;
-		}
-		_putchar('\n');
-	}
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < i; j++)
+            {
+                _putchar(' ');
+            }
+            _putchar('\\');
+            _putchar('\n');
+        }
+    }
 }
