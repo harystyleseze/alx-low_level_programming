c4b1436ac70c7a41ce9714a9a6fcfa91e1b461f7
harystyles <harystyleseze@gmail.com>
2023-09-15 14:11:57 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numberes - entry point
+ * print_most_numbers - entry point
  *
  * Description: prints the numbers, from 0 to 9,
  * except 2 and 4, followed by a new line
