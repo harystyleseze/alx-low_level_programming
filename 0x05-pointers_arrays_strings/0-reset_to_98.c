8505e52954f3ab463c5c561b77263f511ce7b86e
harystyles <harystyleseze@gmail.com>
2023-09-18 18:58:11 -0700

    Reset n to a new number

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the integer n to 98
+ * @n: integer variable to be updated
+ *
+ * Description: function that takes a
+ * pointer to an int as parameter and
+ * updates the value it points to to 98.
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
