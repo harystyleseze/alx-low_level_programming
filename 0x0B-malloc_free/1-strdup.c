19eda0946adbd8180449c88c142172cff1656851
harystyles <harystyleseze@gmail.com>
2023-10-04 11:30:39 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Use malloc to dub=plicate string
+ * @str: specific string
+ * Return: Pointer to newly duped string
+ * or NULL if empty or insufficient
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	int count1, count2;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (count1 = 0; str[count1] != '\0'; count++)
+		;
+
+	array = malloc(count1 * sizeof(*array) + 1);
+	if (array == NULL)
+		return (NULL);
+
+	for (count2 = 0; count2 < i; count2++)
+		array[count2] = str[count2];
+	array[count2] = '\0';
+
+	return (array);
+}
