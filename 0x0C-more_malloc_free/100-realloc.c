2ba0a7355a284d5d462af706069d9be475b501ac
harystyles <harystyleseze@gmail.com>
2023-10-08 14:15:56 -0700

    Updated

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,40 +10,40 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr;
+	char *p;
 	char *old_p;
 	unsigned int i;
 
 	if (new_size == old_size)
-		return (ptr);
+		return (p);
 
-	if (new_size == 0 && ptr)
+	if (new_size == 0 && p)
 	{
-		free(ptr);
+		free(p);
 		return (NULL);
 	}
 
-	if (!ptr)
+	if (!p)
 		return (malloc(new_size));
 
-	ptr = malloc(new_size);
-	if (!ptr)
+	p = malloc(new_size);
+	if (!p)
 		return (NULL);
 
-	old_p = ptr;
+	old_p = p;
 
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			ptr[i] = old_p[i];
+			p[i] = old_p[i];
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr[i] = old_p[i];
+			p[i] = old_p[i];
 	}
 
-	free(ptr);
-	return (ptr);
+	free(p);
+	return (p);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,11 +16,11 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	j = max - min + 1;
-	p = malloc(sizeof(int) * l);
+	p = malloc(sizeof(int) * j);
 
 	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < l; i++, min++)
+	for (i = 0; i < j; i++, min++)
 	{
 		a[i] = min;
 	}
