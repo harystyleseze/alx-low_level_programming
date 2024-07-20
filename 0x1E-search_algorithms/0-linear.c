269a0880ccb4bbe1e745b74887f8777965bd1447
harystyles <harystyleseze@gmail.com>
2024-07-20 15:42:58 -0700

    quick fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (x = 0; x < size; x++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
 		if (array[x] == value)
 		{
 			return (x);
